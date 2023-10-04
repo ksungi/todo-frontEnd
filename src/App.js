a948644183df0bcc28bd64ffc06128a1ea0a82f6
Kim Seong Dong <poiksungi@naver.com>
2023-10-04 13:57:23 +0900

    회원가입 및 로그인 구현완료 및 백과 결합완료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
-import Todo from "./Todo";
+import React from 'react';
+import Todo from './Todo';
 import AddTodo from './AddTodo';
-import {Paper, List, Container} from "@material-ui/core";
+import {Paper, List, Container, Grid, Button, AppBar, Toolbar, Typography} from "@material-ui/core";
 import './App.css';
-import { call } from './service/ApiService';
+import { call, signout } from './service/ApiService';
+
 
 class App extends React.Component {
   constructor(props){ //매개변수 props 생성자
     super(props);   //매개변수 pros 초기화
     this.state ={  // Item 에 item.id, item.done 매개변수 이름과 값 할당
       items: [],
+      /*로딩 중 이라는 상태를 표현할 변수 생성자에 상태 변수를 초기화*/
+      loading: true,
     };
   }
 
@@ -30,10 +33,11 @@ class App extends React.Component {
       this.setState({items: response.data})
       );
   }
-
+  
+  //페이지(DOM) 마운트가 일어나고 렌더링 되기 전에 실행됨
   componentDidMount() {
     call("/todo", "GET", null).then( (response)=>
-      this.setState({items: response.data})
+      this.setState({items: response.data, loading: false})
       );
   }
 
@@ -56,13 +60,48 @@ class App extends React.Component {
       </Paper>
     );
     
-    
-    return (
-      <div className="App"> 
+    //navigationBar
+    var navigationBar = (
+      <AppBar position="static">
+        <Toolbar>
+          <Grid justify="space-between" container>
+            
+            <Grid item>
+              <Typography variant="h6"> 오늘의 할 일</Typography>
+            </Grid>
+            
+            <Grid item>
+              <Button color="inherit" onClick={signout}> logout </Button>
+            </Grid>
+
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    );
+
+    //loading 중이 아닐 때
+    var todoListPage = (
+      <div>
+        {navigationBar}
         <Container maxWidth="md">
-          <AddTodo add = {this.add}/>
-          <div className="TodoList"> {todoItems} </div>
-        </Container> 
+          <AddTodo add = {this.add} />
+          <div className='TodoList'> {todoItems} </div>
+        </Container>
+      </div>
+    );
+    
+    //loading 중일 때
+    var loadingPage = <h1>로딩중...</h1>
+    var content = loadingPage;
+
+    if(!this.state.loading) {
+      content = todoListPage;
+    }
+
+    //생성된 컴포넌트 JPX를 리턴
+    return (
+      <div className="App">
+        {content}  
       </div>
     );
   }
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,18 @@
 import { API_BASE_URL } from "../app-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if(accessToken) {
+        headers.append("Authorization","Bearer " + accessToken);
+    }
+
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -23,10 +31,52 @@ export function call(api, method, request) {
             })
         )
         .catch( (error)=>{
+            console.log("Oops!")
             console.log(error.status);
+            console.log("Oops!")
             if(error.status === 403) {
                 window.location.href = "/login";
             }
             return Promise.reject(error);
         });
+}
+
+//로그인을 위한 API서비스 메소드 SignIn
+export function signin(userDTO) {
+    return call("/auth/signin", "POST", userDTO)
+    .then( (response) => {
+        if(response.token) {
+            //local스토리지에 토큰 저장
+            localStorage.setItem("ACCESS_TOKEN", response.token);
+            //token이 존재하는 경우 todo 화면으로 이동
+            window.location.href = "/";
+        }
+    });
+}
+
+//회원 가입 요청
+export function signup(userDTO) {
+    return call("/auth/signup", "POST", userDTO)
+    .then( (response) => {
+        if(response.id) {
+            window.location.href = "/";
+        }
+    })
+    .catch( (error) => {
+        console.log("Oops!"); 
+        console.log(error.status); 
+        console.log("Ooops!")
+        
+        if(error.status === 403) { 
+            window.location.href = "/auth/signup"; 
+        } 
+        return Promise.reject(error);
+    });
+}
+
+//로그아웃
+export function signout() {
+    //local 스토리지에 토큰 삭제
+    localStorage.setItem("ACCESS_TOKEN", null);
+    window.location.href = "/";
 }
