af3db63047e9609a28aa1bc5e8f3ee7e73485314
ksungi <144586495+ksungi@users.noreply.github.com>
2023-10-04 16:35:33 +0900

    app-config 수정
    본인 기기를 서버로 활용하기 위한 URL지정

--- src/app-config.js
+++ src/app-config.js
@@ -6,5 +6,9 @@ console.log("hostname: ", hostname);
 if(hostname === "localhost") {
     backendHost = "http://localhost:8080";
 }
+// else {
+//     backendHost = "http://172.30.122.189:8080";
+// }
+// 백엔드호스트에 ipconfig > ipv4 주소를 입력하면 같은 네트워크 상에서는 가능 
 
 export const API_BASE_URL = `${backendHost}`;
