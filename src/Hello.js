96f35c5d24b797eb25c1a8d0de0e25edeb7d77b9
ksungi <144586495+ksungi@users.noreply.github.com>
2023-10-06 13:11:31 +0900

    Hello/Login.js_class->func_완료

--- src/Hello.js
+++ src/Hello.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-class Hello extends React.Component {
-    render() {
-        return (
-            <div className="Hello">
-                <label for="hello"> 안녕하세요! </label>
-            </div>
-        );
-    }
+function Hello(props) {
+    return (
+        <div className="Hello">
+            <label for="hello"> 안녕하세요! </label>
+        </div>
+    );
 }
 
 export default Hello;
--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { signin } from "./service/ApiService";
 import { Button, TextField, Grid, Link, Container, Typography } from "@material-ui/core";
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.state={ showPassword: false };
-    }
+function Login(props) {
+    const [showPassword, setShowPassword] = useState();
 
-    handleSubmit(event) {
+    const handleSubmit = (event)=>{
         event.preventDefault();
         
         const data = new FormData(event.target);
@@ -20,55 +16,51 @@ class Login extends React.Component {
         signin( {email: email, password: password} );
     }
 
-    render() {
-        const showpassword = this.state.showPassword;
-        return (
-            
-            <Container component="main" maxWidth="xs" style={ {marginTop: "8%"} }>
+    return (
+        <Container component="main" maxWidth="xs" style={ {marginTop: "8%"} }>
+            <Grid container spacing={2}>
+                <Typography component="h1" variant="h5">
+                    로그인
+                </Typography>
+            </Grid>
+            <form noValidate onSubmit={handleSubmit}>
+                { " " }
+                { /*submit버튼을 누르면 handleSubmit이 실행됨*/}
                 <Grid container spacing={2}>
-                    <Typography component="h1" variant="h5">
-                        로그인
-                    </Typography>
-                </Grid>
-                <form noValidate onSubmit={this.handleSubmit}>
-                    { " " }
-                    { /*submit버튼을 누르면 handleSubmit이 실행됨*/}
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField  variant="outlined"
-                                        required fullWidth
-                                        id="email"
-                                        label="이메일 주소"
-                                        name="email"
-                                        autoComplete="email"/>
-                        </Grid>
-                        <Grid item xs={12}>
-                            {/* 비밀번호 가림 ON/OFF 토글 넣지 못함 */}
-                            <TextField  type={ showpassword ? "text" : "password" }
-                                        variant="outlined"
-                                        required fullWidth
-                                        id="password"
-                                        label="패스워드"
-                                        name="password"
-                                        autoComplete="password"/>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Button type="submit"    
-                                    fullWidth
-                                    variant="contained"
-                                    color="primary"> 로그인 
-                            </Button>
-                        </Grid>
-                        <Link href="/signup" variant="body2">
-                            <Grid item>
-                                계정이 없습니까? 여기서 가입하세요. 
-                            </Grid>
-                        </Link>
+                    <Grid item xs={12}>
+                        <TextField  variant="outlined"
+                                    required fullWidth
+                                    id="email"
+                                    label="이메일 주소"
+                                    name="email"
+                                    autoComplete="email"/>
                     </Grid>
-                </form>
-            </Container>
-        );
-    }
+                    <Grid item xs={12}>
+                        {/* 비밀번호 가림 ON/OFF 토글 넣지 못함 */}
+                        <TextField  type={ setShowPassword ? "text" : "password" }
+                                    variant="outlined"
+                                    required fullWidth
+                                    id="password"
+                                    label="패스워드"
+                                    name="password"
+                                    autoComplete="password"/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button type="submit"    
+                                fullWidth
+                                variant="contained"
+                                color="primary"> 로그인 
+                        </Button>
+                    </Grid>
+                    <Link href="/signup" variant="body2">
+                        <Grid item>
+                            계정이 없습니까? 여기서 가입하세요. 
+                        </Grid>
+                    </Link>
+                </Grid>
+            </form>
+        </Container>
+    );
 }
 
 export default Login;
